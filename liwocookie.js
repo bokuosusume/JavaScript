f33490c66f82abc9ed807971376369b7d050fb4a
Sams <55945418+bokuosusume@users.noreply.github.com>
2020-06-11 10:47:34 +0800

    Update liwocookie.js

--- liwocookie.js
+++ liwocookie.js
@@ -1,14 +1,3 @@
-/*
-进野比大佬的 bean.m.jd.com 再签到一下就能获取cookie了。
-【Loon 2.1+ 脚本配置】:
----------------------------
-[Script]
-cron "0 */1 * * *" tag=京东梨涡任务查看, script-path=https://raw.githubusercontent.com/bokuosusume/JavaScript/master/liwotaskview.js
-http-request https:\/\/api\.m\.jd\.com\/client\.action.*functionId=signBean tag=获取京东Cookie, script-path=https://raw.githubusercontent.com/bokuosusume/JavaScript/master/liwocookie.js
-[MITM]
-hostname = api.m.jd.com
-
-*/
 
 
 const CookieName = '梨涡任务查看'
