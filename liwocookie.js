fbd59250f3f89cd82363dddaf2eb4d13a19c2128
Sams <55945418+bokuosusume@users.noreply.github.com>
2020-06-11 10:31:08 +0800

    Update liwocookie.js

--- liwocookie.js
+++ liwocookie.js
@@ -1,11 +1,13 @@
 /*
+
 【Loon 2.1+ 脚本配置】:
-*************************
+---------------------------
 [Script]
-cron "0 */1 * * *" tag=京东梨涡任务查看, script-path=https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js
-http-request https:\/\/api\.m\.jd\.com\/client\.action.*functionId=signBean tag=获取京东Cookie, script-path=https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js
+cron "0 */1 * * *" tag=京东梨涡任务查看, script-path=https://raw.githubusercontent.com/bokuosusume/JavaScript/master/liwotaskview.js
+http-request https:\/\/api\.m\.jd\.com\/client\.action.*functionId=signBean tag=获取京东Cookie, script-path=https://raw.githubusercontent.com/bokuosusume/JavaScript/master/liwocookie.js
 [MITM]
 hostname = api.m.jd.com
+
 */
 
 
