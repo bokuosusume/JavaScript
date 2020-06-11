1c1e087ddfcca248d463489098f320265eefd1e1
Sams <55945418+bokuosusume@users.noreply.github.com>
2020-06-11 10:24:44 +0800

    Create liwocookie.js
    liwo APP

---
+++ liwocookie.js
@@ -0,0 +1,27 @@
+/*
+【Loon 2.1+ 脚本配置】:
+*************************
+[Script]
+cron "0 */1 * * *" tag=京东梨涡任务查看, script-path=https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js
+http-request https:\/\/api\.m\.jd\.com\/client\.action.*functionId=signBean tag=获取京东Cookie, script-path=https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js
+[MITM]
+hostname = api.m.jd.com
+*/
+
+
+const CookieName = '梨涡任务查看'
+const Key = 'CookieJD'
+const Val = $request.headers['Cookie']
+
+if (Val){
+  let cookie = $persistentStore.write(Val, Key)
+  let msg = `${CookieName}`
+  if (cookie){
+    $notification.post(msg, '京东cookie写入成功', '详见日志')
+    console.log(msg)
+    console.log(Val)
+    console.log("thanks")
+    $done({})
+    
+  }
+}
