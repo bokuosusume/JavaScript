6b767a88ff040fde985b0acec6bfb91c6f805dd2
Sams <55945418+bokuosusume@users.noreply.github.com>
2020-06-12 17:09:54 +0800

    Update liwocookie.js

--- liwocookie.js
+++ liwocookie.js
@@ -1,17 +1,17 @@
 /**
-* 邀请链接 http://2do.jd.com/events/red-envelopes/?inviter=1236228340192960513&channel=cash&extParam=1260048962852974594#/
-* 进野比大佬的京东京豆签到 bean.m.jd.com 再签到一下就能获取cookie了。
-*
-* 【Loon 2.1+ 脚本配置】:
-*
-* [Script]
-*
-* cron "0 */1 * * *" tag=京东梨涡任务查看, script-path=https://raw.githubusercontent.com/bokuosusume/JavaScript/master/liwotaskview.js http-request https://api.m.jd.com/client.action.*functionId=signBean tag=获取京东Cookie, script-path=https://raw.githubusercontent.com/bokuosusume/JavaScript/master/liwocookie.js
-*
-* [MITM]
-*
-* hostname = api.m.jd.com
-**/
+ * 邀请链接 http://2do.jd.com/events/red-envelopes/?inviter=1236228340192960513&channel=cash&extParam=1260048962852974594#/
+ * 进野比大佬的京东京豆签到 bean.m.jd.com 再签到一下就能获取cookie了。
+ *
+ * 【Loon 2.1+ 脚本配置】:
+ *
+ * [Script]
+ *
+ * cron "0 */1 * * *" tag=京东梨涡任务查看, script-path=https://raw.githubusercontent.com/bokuosusume/JavaScript/master/liwotaskview.js http-request https://api.m.jd.com/client.action.*functionId=signBean tag=获取京东Cookie, script-path=https://raw.githubusercontent.com/bokuosusume/JavaScript/master/liwocookie.js
+ *
+ * [MITM]
+ *
+ * hostname = api.m.jd.com
+ */
 
 const CookieName = '梨涡任务查看'
 const Key = 'CookieJD'
