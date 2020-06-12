8e3223bce5324a30d4253310c4a78e231c51c5b8
Sams <55945418+bokuosusume@users.noreply.github.com>
2020-06-12 17:12:03 +0800

    Update liwocookie.js

--- liwocookie.js
+++ liwocookie.js
@@ -1,4 +1,5 @@
 /**
+ * 我的梨涡邀请码：dasaw
  * 邀请链接 http://2do.jd.com/events/red-envelopes/?inviter=1236228340192960513&channel=cash&extParam=1260048962852974594#/
  * 进野比大佬的京东京豆签到 bean.m.jd.com 再签到一下就能获取cookie了。
  *
