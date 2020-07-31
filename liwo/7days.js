79490b7169c747c53a14e8fa904c1603c95a6582
Sams <55945418+bokuosusume@users.noreply.github.com>
2020-07-31 17:46:58 +0800

    Update 7days.js

--- liwo/7days.js
+++ liwo/7days.js
@@ -19,9 +19,10 @@ const lwbody = $persistentStore.read(lwbodyKey)
 
 const headers = {"Accept": "application/json, text/plain, */*","Accept-Encoding": "gzip, deflate, br","Accept-Language": "zh-cn","Connection": "keep-alive","Content-Length": "246","Content-Type": "application/x-www-form-urlencoded","Cookie": lwVal,"Host": "api.m.jd.com","Origin": "https://2do.jd.com","Referer": "https://2do.jd.com/events/7-days/?sid=c2517ef919393ab4646c062ae0fae62w","User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148/yocial,"}
 
-//获取自1970年1月1日00:00:00 UTC以来经过的毫秒数
+//获取自1970年1月1日00:00:00 UTC以来经过的毫秒数并更新到消息体内
 const nowtime = Date.now()
 const changebody = lwbody.replace(/(&t=)\d*/,"&t=" + nowtime)
+console.log("刷新时间成功 "+"Time:" + nowtime + "Body:"changebody)
 
 var params = {
     url:"https://api.m.jd.com/api/v1/sign/doSign",
