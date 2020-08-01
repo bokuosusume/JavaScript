e99184bde1026ecc1dcd9106ca1ea9cee40c21cb
Sams <55945418+bokuosusume@users.noreply.github.com>
2020-08-01 12:01:33 +0800

    支持QX loon surge

--- liwo/7dayscookie.js
+++ liwo/7dayscookie.js
@@ -10,19 +10,20 @@
  *hostname = api.m.jd.com
  */
  
- 
+ //支持QX loon surge
 const CookieName = '😀梨涡签到领钱'
+const sams = init()
 const lwKey = 'CookieJD'
 const lwVal = $request.headers['Cookie']
 const lwbody = $request.body
 const lwbodyKey = "Body"
 
 if (lwVal && lwbody){
-  let cookie = $persistentStore.write(lwVal, lwKey)
-  let body = $persistentStore.write(lwbody, lwbodyKey)
+  let cookie = sams.setdata(lwVal, lwKey)
+  let body = sams.setdata(lwbody, lwbodyKey)
   let msg = `${CookieName}`
   if (cookie && body){
-    $notification.post(msg, '❤梨涡签到Body and Cookie写入成功', '详见日志')
+    sams.msg(msg, '❤梨涡签到Body and Cookie写入成功', '详见日志')
     console.log(msg)
     console.log(lwVal)
     console.log(lwbody)
@@ -31,5 +32,57 @@ if (lwVal && lwbody){
   }
 }
 
-
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, resp, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, resp, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return {
+    isSurge,
+    isQuanX,
+    msg,
+    log,
+    getdata,
+    setdata,
+    get,
+    post,
+    done
+  }
+}
 //https:\/\/api\.m\.jd\.com\/api\/v1\/sign\/doSign
